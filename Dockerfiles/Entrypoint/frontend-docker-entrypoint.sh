f9168c1b8b840387d57f4d219fe563165d435190
team-cloudboost <team.cloudboost@gmail.com>
2022-11-29 22:04:58 +0600

    Glue Updated

--- Dockerfiles/Entrypoint/frontend-docker-entrypoint.sh
+++ Dockerfiles/Entrypoint/frontend-docker-entrypoint.sh
@@ -7,6 +7,6 @@ sed -i "s,fastcgi_pass.*,fastcgi_pass $BACK_HOST:$BACK_HOST_PORT;,g" /etc/nginx/
 sed -i "s,DB_DATABASE.*,DB_DATABASE=$APP_DB,g" /public_html/$REPO_PROJECT_FOLDER/.env;
 sed -i "s,DB_USERNAME.*,DB_USERNAME=$APP_DB_USER,g" /public_html/$REPO_PROJECT_FOLDER/.env;
 sed -i "s,DB_PASSWORD.*,DB_PASSWORD=$APP_DB_PASS,g" /public_html/$REPO_PROJECT_FOLDER/.env;
-sed -i "s,DB_HOST.*,DB_HOST=$DB_HOST,g" /public_html/$REPO_PROJECT_FOLDER/.env;
+sed -i "s,DB_HOST.*,DB_HOST=$APP_DB_HOST,g" /public_html/$REPO_PROJECT_FOLDER/.env;
 sed -i "s,DB_PORT.*,DB_PORT=$APP_DB_PORT,g" /public_html/$REPO_PROJECT_FOLDER/.env;
 nginx -g 'daemon off;';
